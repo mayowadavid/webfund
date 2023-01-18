bdbc72cc2fc630f6dd227b41d0caa45361857f93
mayowadavid <mayowadavid100@gmail.com>
2023-01-19 00:35:33 +0100

    update footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -8,42 +8,28 @@
             class="md:w-1/5 w-1/2 md:mb-0 mb-3"
             :key="index"
           >
+          <a :href="item.link"
+                class="text-[#9598A3]"
+                >
             <h4 class="tetx-lg font-bold text-[#575B68] mb-2">
               {{ item.title }}
-            </h4>
-            <div class="flex flex-col space-y-3">
-              <a
-                v-for="(item_link, i) in item.links"
-                :key="i"
-                :href="item_link.link"
-                class="text-[#9598A3]"
-                >{{ item_link.menu }}</a
-              >
-            </div>
+            </h4></a>
           </div>
         </div>
         <div class="flex flex-wrap justify-start items-start mb-5">
           <div
             v-for="(item, index) in menu_items_2"
             class="md:w-1/5 w-1/2 md:mb-0 mb-3"
-            :key="index"
-          >
+            :key="index">
+            <a
+            :href="item.link"
+            class="text-[#9598A3]"
+            >
             <h4 class="tetx-lg font-bold text-[#575B68] mb-2">
               {{ item.title }}
-            </h4>
-            <div class="flex flex-col space-y-3">
-              <a
-                v-for="(item_link, i) in item.links"
-                :key="i"
-                :href="item_link.link"
-                class="text-[#9598A3]"
-                >{{ item_link.menu }}</a
-              >
-            </div>
+            </h4></a>
           </div>
-          <div
-            class="md:w-1/5 w-1/2 md:mb-0 mb-3"
-          >
+          <div class="md:w-1/5 w-1/2 md:mb-0 mb-3">
             <h4 class="tetx-lg font-bold text-[#575B68] mb-2">
               {{ contact_items.title }}
             </h4>
@@ -85,66 +71,15 @@ export default {
       show: false,
       year: "",
       menu_items_1: [
-        {
-          title: 'Our Solution',
-          links: [
-            { menu: 'NGOs', link: '/solution' },
-            { menu: 'Churches & Mosques', link: '/solution' },
-            { menu: 'Political Organization', link: '/solution' },
-            { menu: 'Government Agencies', link: '/solution' },
-            { menu: 'Student Bodies', link: '/solution' },
-            { menu: 'Private Organization', link: '/solution' },
-          ],
-        },
-        {
-          title: 'Company',
-          links: [
-            { menu: 'Our Stories', link: '/company' },
-            { menu: 'Our Value', link: '/company' },
-            { menu: 'Our Mission', link: '/company'  },
-            { menu: 'Our Vision', link: '/company'  },
-            { menu: 'Our Team', link: '/company'  },
-          ],
-        },
-        {
-          title: 'Legal',
-          links: [
-            { menu: 'Terms & Condition', link: '/terms' },
-            { menu: 'Privacy Policy', link: '/privacy' },
-            { menu: 'Wefundx usage Agreement', link: '#' },
-            { menu: 'Donation Protection Guarantee', link: '#' },
-          ],
-        },
-        {
-          title: 'Pricing',
-          links: [
-            { menu: 'Processing fee for donors', link: '/pricing' },
-            { menu: 'Processing fee for organization', link: '/pricing' },
-          ],
-        },
-        {
-          title: 'Support',
-          links: [
-            { menu: 'Help desk', link: '/support' },
-            { menu: 'Contact Us', link: '/support' },
-          ],
-        },
+        { title: 'Our Solution', link: '/solution' },
+        { title: 'Company', link: '/company' },
+        { title: 'Legal', link: '/terms' },
+        { title: 'Pricing', link: '/pricing' },
+        { title: 'Support', link: '/support' },
       ],
       menu_items_2: [
-        {
-          title: 'Developers',
-          links: [
-            { menu: 'Overview', link: '/developers' },
-            { menu: 'Documentation', link: '/documentation' },
-          ],
-        },
-        {
-          title: 'Learn',
-          links: [
-            { menu: 'Tutorial Videos', link: '/company' },
-            { menu: 'Sitemap', link: '/company' },
-          ],
-        },
+        { title: 'Developers', link: '/developers' },
+        { title: 'Learn', link: '/company' },
       ],
       contact_items: {
         title: 'Contact',
